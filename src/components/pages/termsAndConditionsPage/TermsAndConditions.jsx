4eb4d85386edc85500b337516d7c3dd529a245ba
drishyap <drishya.pou@gmail.com>
2025-03-17 23:07:00 +0545

    update products

--- src/components/pages/termsAndConditionsPage/TermsAndConditions.jsx
+++ src/components/pages/termsAndConditionsPage/TermsAndConditions.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import CustomBreadCrumbs from "../../common/CustomBreadcrumbs/CustomBreadcrumbs";
 import ScrollToTop from "../../common/scrollToTop/ScrollToTop";
 
 const TermsAndConditions = () => {
    const { siteSettings } = useSelector((state) => state?.utils);
+
    return (
       <>
          <ScrollToTop />
+         <CustomBreadCrumbs />
          <div className="containerWrap">
             <div className="containerBody">
                {" "}
-               <div className="mb-[60px]">
-                  <div className="h-[180px] md:h-[250px] mb-[30px] relative overflow-hidden">
-                     <div className="absolute inset-0 bg-[#24639e] transform -skew-y-6 origin-top-left"></div>
-                     <div className="relative flex items-center h-[100%] z-10 pl-[15px] md:pl-[30px] text-[18px] md:text-[28px] font-[600] text-[#fff]">
-                        Terms & Conditions
-                     </div>
-                  </div>{" "}
+               <div className="my-[60px]">
+                  <div className="text-[40px] font-[700] mb-[30px]">
+                     Terms And Conditions{" "}
+                  </div>
                   <div
                      className="text-[18px]"
                      dangerouslySetInnerHTML={{
